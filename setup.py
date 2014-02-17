22c73afe207cdeb8ef0c4356e2344cf3bdcc923c
Dmitry Ivanchenko <dmitry.ivanchenko@zojax.com>
2014-02-17 16:46:11 +0200

    #589 add tests

--- setup.py
+++ setup.py
@@ -18,50 +18,61 @@ $Id$
 import sys, os
 from setuptools import setup, find_packages
 
+
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version='0'
 
+version = '0'
 
-setup(name = 'zojax.principal.rpxnow',
-      version = version,
-      description = "RpxNow authentication module for zojax.",
-      long_description = (
+setup(name='zojax.principal.rpxnow',
+      version=version,
+      description="RpxNow authentication module for zojax.",
+      long_description=(
           'Detailed Documentation\n' +
           '======================\n'
           + '\n\n' +
           read('CHANGES.txt')
-          ),
-      classifiers = [
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Zope Public License',
-        'Programming Language :: Python',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP',
-        'Framework :: Zope3'],
-      keywords = 'Zope3, RpxNow',
-      author = 'Anatoly Bubenkov',
-      author_email = 'bubenkoff@gmail.com',
-      url = '',
-      license = 'ZPL 2.1',
-      include_package_data = True,
-      zip_safe = False,
-      packages = find_packages('src'),
-      package_dir = {'':'src'},
-      namespace_packages = ['zojax', 'zojax.principal'],
-      install_requires = ['setuptools',
-                          'simplejson',
-                          'zope.session',
-                          'zope.cachedescriptors',
-                          'zope.app.container',
-                          'zojax.authentication',
-                          'zojax.statusmessage',
-                          'zojax.principal.registration',
-                          ],
-      extras_require = dict(test = ['zope.app.testing',
-                                    ])
-      )
+      ),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      keywords='Zope3, RpxNow',
+      author='Anatoly Bubenkov',
+      author_email='bubenkoff@gmail.com',
+      url='',
+      license='ZPL 2.1',
+      include_package_data=True,
+      zip_safe=False,
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['zojax', 'zojax.principal'],
+      install_requires=['setuptools',
+                        'simplejson',
+                        'zope.session',
+                        'zope.cachedescriptors',
+                        'zope.app.container',
+                        'zojax.authentication',
+                        'zojax.statusmessage',
+                        'zojax.principal.registration',
+                        'zojax.product',
+                        'zojax.portlet',
+                        'zojax.pageelement',
+      ],
+      extras_require=dict(test=['zope.app.testing',
+                                'zope.app.zcmlfiles',
+                                'zope.testing',
+                                'zope.testbrowser',
+                                'zope.securitypolicy',
+                                'zojax.autoinclude',
+                                'zojax.security',
+
+      ])
+)
